0831dc824c93cec1da4d93fd365d93b863586b3a
MatteaAnn Swain <MMSwain@tql.com>
2021-06-18 11:52:03 -0400

    backend to get all users

--- js/counter.js
+++ js/counter.js
@@ -12,6 +12,8 @@ $( () =>
       let inpCtrl = $("#counter") // gets access to value
       inpCtrl.val(counter) // tkaes counter and stores it in controller
         inpCtrl.css("color", counter % 2 == 0 ? "red" : "black"); 
+        inpCtrl.css("fontStyle", counter % 3 == 0 ? "italic" : "normal");
+        inpCtrl.css("fontWeight", counter % 7 == 0 ? "bold" : "normal");
 
     };
 
---
+++ js/prs-ajax.js
@@ -0,0 +1,49 @@
+let users = [];
+
+// jQuery Ready function 
+$(() => // annoymous function 
+{
+    $.getJSON("http://localhost:30000/api/Users") // making a call to get all users
+    // .done (var for what is returning)
+        .done((res) =>  // when that is done do this with data
+        {// var users only exists in this done method. Store data somewhere outside 
+            console.debug(res); // debug only shows in verbose 
+            users = res; // store results in empty array users
+            display(); // need to display data when data has returned 
+        }) 
+    // if failed (var for what is returning)    
+        .fail((err) => 
+        {
+            console.error(err); 
+        }); // write error message in console 
+});
+
+const display = () =>
+{
+    let dataCtrl = $("#data")
+    dataCtrl.val(users)
+    for(let u of users)
+    {
+        let tr = $("<tr> </tr>");
+        let tdId = $(`<td> ${u.id}</td>`);
+        tr.append(tdId); 
+
+        let tdUsername = $(`<td> ${u.username} </td>`);
+        tr.append(tdUsername);
+
+        let tdFirstname = $(`<td> ${u.firstname} </td>`);
+        tr.append(tdFirstname);
+
+        let tdLastname = $(`<td> ${u.lastname} </td>`);
+        tr.append(tdLastname);
+
+        let tdPhone = $(`<td> ${u.phone} </td>`);
+        tr.append(tdPhone);
+
+        let tdEmail = $(`<td> ${u.email} </td>`);
+        tr.append(tdEmail);
+
+        dataCtrl.append(tr);
+    };
+};
+
